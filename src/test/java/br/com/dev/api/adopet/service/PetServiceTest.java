00f98d5611b801946944fb25edf93c05a95684f2
unknown <arturilenwood@gmail.com>
2024-04-25 21:57:41 -0300

    Criação novos testes pet service

--- src/test/java/br/com/dev/api/adopet/service/PetServiceTest.java
+++ src/test/java/br/com/dev/api/adopet/service/PetServiceTest.java
@@ -1,40 +1,113 @@
 package br.com.dev.api.adopet.service;
 
+import br.com.dev.api.adopet.dto.pet.PetPageDto;
+import br.com.dev.api.adopet.dto.pet.PetRequestDto;
+import br.com.dev.api.adopet.infra.exception.PetNotFoundException;
+import br.com.dev.api.adopet.model.abrigo.Abrigo;
 import br.com.dev.api.adopet.model.pet.Pet;
 import br.com.dev.api.adopet.repository.PetRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.when;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class PetServiceTest {
-    @InjectMocks
-    private PetService petService;
     @Mock
     private PetRepository petRepository;
-    @Mock
-    private Pageable pageable;
-    @Mock
-    private Page<Pet> allByAdotadoFalse;
+    @InjectMocks
+    private PetService petService;
+    private static final Long PET_ID = 1L;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
-    @DisplayName("Deveria retornar page com todos os pets disponiveis")
+    @DisplayName("Deveria retornar um Page de pets")
     void scenario01() {
         // Arrange
-        when(petRepository.findAllByAdotadoFalse(pageable)).thenReturn(allByAdotadoFalse);
+        Page<Pet> petsPage = mock(Page.class);
+        Pageable pageable = mock(Pageable.class);
+        when(petRepository.findAllByAdotadoFalse(pageable)).thenReturn(petsPage);
 
         // Act
-        petService.listar(pageable);
+        Page<PetPageDto> result = petService.listar(pageable);
 
         // Assert
-        then(petRepository).should().findAllByAdotadoFalse(pageable);
+        verify(petRepository).findAllByAdotadoFalse(pageable);
+    }
+
+    @Test
+    @DisplayName("Deveria detalhar um pet pelo ID")
+    void scenario02() {
+        // Arrange
+        Pet pet = new Pet();
+        when(petRepository.findById(PET_ID)).thenReturn(Optional.of(pet));
+
+        // Act
+        Pet result = petService.detalhar(PET_ID);
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(pet, result);
+        verify(petRepository).findById(PET_ID);
+    }
+
+    @Test
+    @DisplayName("Deveria lancar PetNotFoundException com um ID invalido")
+    void scenario03() {
+        // Arrange
+        when(petRepository.findById(PET_ID)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(PetNotFoundException.class, () -> petService.detalhar(PET_ID));
+        verify(petRepository).findById(PET_ID);
+    }
+
+    @Test
+    @DisplayName("Deveria retornar um Page de pets em um abrigo pelo ID")
+    void scenario04() {
+        // Arrange
+        Abrigo abrigo = mock(Abrigo.class);
+        Page<Pet> petsPage = mock(Page.class);
+        Pageable pageable = mock(Pageable.class);
+        when(petRepository.findByAbrigoAndAdotadoFalse(abrigo, pageable)).thenReturn(petsPage);
+
+        // Act
+        var result = petService.listarPetsPorAbrigo(abrigo, pageable);
+
+        // Assert
+        verify(petRepository).findByAbrigoAndAdotadoFalse(abrigo, pageable);
+    }
+
+    @Test
+    @DisplayName("Deveria cadastrar um pet")
+    void scenario05() {
+        // Arrange
+        Abrigo abrigo = mock(Abrigo.class);
+        PetRequestDto dto = mock(PetRequestDto.class);
+        Pet pet = mock(Pet.class);
+        when(petRepository.save(any())).thenReturn(pet);
+
+        // Act
+        var result = petService.cadastrar(abrigo, dto);
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(pet, result);
+        verify(petRepository).save(any());
     }
 }
